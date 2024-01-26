23cc70ee5b1dc776eeadb405fd9b56127f89d78a
SerrySaurav <sauravkumar080999@gmail.com>
2024-01-26 14:29:12 +0530

    implemented game card skeleton

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react'
+import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react'
 import React from 'react'
 import PlatformIconList from './PlatformIconList'
 import CriticScore from './CriticScore'
@@ -6,7 +6,7 @@ import getCroppedImageUrl from '../services/image-url'
 
 const GameCard = ({game}) => {
   return (
-    <Card borderRadius={10} overflow={'hidden'}>
+    <Card  borderRadius={10} overflow={'hidden'}>
       <Image src={getCroppedImageUrl(game.background_image)}/>
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
--- src/hooks/useGames.jsx
+++ src/hooks/useGames.jsx
@@ -3,22 +3,27 @@ import apiClient from '../services/api-client';
 
 const useGames = () => {
  
-  const [games, setGames] = useState([]);
+ const [games, setGames] = useState([]);
  const [error, setError] = useState('');
+ const [loading, setLoading] = useState(false);
 
  useEffect(() => {
 
-
+   setLoading(true)
    apiClient.get('/games')
     .then(res => {
         setGames(res.data.results)
         console.log(res.data.results)
+        setLoading(false)
     })
-    .catch(err => setError(err))
+    .catch(err => {
+      setError(err)
+      setLoading(false)
+    })
  
  },[])
 
- return {games, error}
+ return {games, error, loading}
 
 }
 
