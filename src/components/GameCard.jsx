104c89ed21c1e728f588a784b04c70a8a2581dfa
SerrySaurav <sauravkumar080999@gmail.com>
2024-02-04 10:54:34 +0530

    improved game card look

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -9,11 +9,11 @@ const GameCard = ({game}) => {
     <Card  borderRadius={10} overflow={'hidden'}>
       <Image src={getCroppedImageUrl(game.background_image)}/>
       <CardBody>
-        <Heading fontSize='2xl'>{game.name}</Heading>
-        <HStack justifyContent='space-between'>
+        <HStack justifyContent='space-between' marginBottom={3}>
           <PlatformIconList platforms={game.parent_platforms}/>
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize='2xl'>{game.name}</Heading>
       </CardBody>
     </Card>
   )
