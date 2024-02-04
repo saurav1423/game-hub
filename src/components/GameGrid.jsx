0480a5175156b4831ed5d2f1d8e303213f7ee7d8
SerrySaurav <sauravkumar080999@gmail.com>
2024-02-04 17:04:06 +0530

    theme change

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -7,17 +7,16 @@ import GameCardSkeleton from './GameCardSkeleton';
 const GameGrid = ({gameQuery}) => {
     const {data, error, loading} = useGames(gameQuery);
     const skeletons = [1,2,3,4,5,6]
+
+  if (error) return <Text>{error}</Text>
     
   return (
-    <>
-     {error && <Text>{error}</Text>}
      <SimpleGrid columns={{sm:1, md:2, lg:3, xl:5}} padding={10} spacing={5}>
       {loading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
         {data.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
      </SimpleGrid>
-    </>
   )
 }
 
